eb740c1aa3962c546e19f048b01e96c997eac724
George <g.peck.iii@gmail.com>
2017-04-24 20:32:04 -0700

    scroll reveal

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,4 +1,6 @@
+import $ from "jquery";
 import MobileMenu from './modules/MobileMenu';
-
+import RevealOnScroll from './modules/RevealOnScroll';
 var mobileMenu = new MobileMenu();
-
+new RevealOnScroll($(".featureItem"),"85%");
+new RevealOnScroll($(".reviews"),"60%");
