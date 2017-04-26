09b605429010ae812da4ed8021b2e979d6748485
George <g.peck.iii@gmail.com>
2017-04-25 20:44:12 -0700

    complete sticky header

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,6 +1,8 @@
 import $ from "jquery";
 import MobileMenu from './modules/MobileMenu';
 import RevealOnScroll from './modules/RevealOnScroll';
+import StickyHeader from './modules/StickyHeader';
 var mobileMenu = new MobileMenu();
 new RevealOnScroll($(".featureItem"),"85%");
 new RevealOnScroll($(".reviews"),"60%");
+var stickyHeader=new StickyHeader();
